ca3624e7de56272f54cb8bff7fc2f032d8fea8b7
Luciano Camargo Cruz <luciano@lccruz.net>
2025-05-30 06:03:52 -0300

    Fix Rest

---
+++ callsystem/tasks/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import Task
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = ['id', 'title', 'description', 'date', 'owner', 'status']
+        read_only_fields = ['id']
--- callsystem/tasks/views.py
+++ callsystem/tasks/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Task
+from .serializers import TaskSerializer
 
-# Create your views here.
+
+@api_view(['GET'])
+def get_tasks(request):
+    tasks = Task.objects.filter(owner=request.user)
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
+
