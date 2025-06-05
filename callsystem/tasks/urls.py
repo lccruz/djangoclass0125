9b638323acac3fa6d658b5d10724404b9998ddd2
Luciano Camargo Cruz <luciano@lccruz.net>
2025-06-05 05:53:04 -0300

    Add task creation endpoint and fix response errors in views

--- callsystem/tasks/urls.py
+++ callsystem/tasks/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('tasks/', views.get_tasks, name='get_tasks'),
+    path('task/', views.create_task, name='create_task'),
 ]
 
--- callsystem/tasks/views.py
+++ callsystem/tasks/views.py
@@ -1,5 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Task
 from .serializers import TaskSerializer
 
@@ -9,4 +10,14 @@ def get_tasks(request):
     tasks = Task.objects.filter(owner=request.user)
     serializer = TaskSerializer(tasks, many=True)
     return Response(serializer.data)
+
+
+@api_view(['POST'])
+def create_task(request):
+    serializer = TaskSerializer(data=request.data)
+    if serializer.is_valid():
+        task = serializer.save()
+        task.owner.add(request.user)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Reponse(serializer.erros, status=status.HTTP_400_BAD_REQUEST)
 
